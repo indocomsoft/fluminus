16e7dc9635a61932ac4665bd52d5d2a061c586cc
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-02-01 13:09:47 +0800

    Add travis, inch-ci and coveralls

--- mix.exs
+++ mix.exs
@@ -4,10 +4,17 @@ defmodule Fluminus.MixProject do
   def project do
     [
       app: :fluminus,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -24,7 +31,9 @@ defmodule Fluminus.MixProject do
       {:httpoison, "~> 1.4"},
       {:jason, "~> 1.1"},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:inch_ex, "~> 2.0", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]

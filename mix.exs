10e239d7c66223e129b0a7e74d8a67882fc91075
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-02-03 11:59:42 +0800

    Add ex_doc as dependency

--- mix.exs
+++ mix.exs
@@ -32,9 +32,10 @@ defmodule Fluminus.MixProject do
       {:html_entities, "~> 0.4"},
       {:httpoison, "~> 1.4"},
       {:jason, "~> 1.1"},
-      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:inch_ex, "~> 2.0", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},

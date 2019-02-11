9c292d55096f010d7a0fb41d7bd42f378994f948
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-02-11 18:47:00 +0800

    Add saving credentials functionality

--- lib/fluminus/cli.ex
+++ lib/fluminus/cli.ex
@@ -3,20 +3,24 @@ defmodule Fluminus.CLI do
   Provides functions related to Fluminus' CLI.
   """
 
+  @config_file "config.json"
+
   alias Fluminus.API.File
   alias Fluminus.Authorization
 
   def run(args) do
     HTTPoison.start()
-    username = IO.gets("username: ") |> String.trim()
-    password = password_get("password: ") |> String.trim()
+    {username, password} = load_credentials()
 
     case Authorization.jwt(username, password) do
       {:ok, auth} ->
+        save_credentials(username, password)
         run(args, auth)
 
       {:error, :invalid_credentials} ->
         IO.puts("Invalid credentials!")
+        clear_credentials()
+        run(args)
 
       {:error, error} ->
         IO.puts("Error: #{inspect(error)}")
@@ -57,6 +61,57 @@ defmodule Fluminus.CLI do
         mod |> File.from_module(auth) |> list_file(auth)
         IO.puts("")
       end
+    end
+  end
+
+  defp load_credentials do
+    with {:ok, data} <- Elixir.File.read(@config_file),
+         {:ok, decoded} <- Jason.decode(data) do
+      {decoded["username"], decoded["password"]}
+    else
+      _ ->
+        username = IO.gets("username: ") |> String.trim()
+        password = password_get("password: ") |> String.trim()
+        {username, password}
+    end
+  end
+
+  def clear_credentials do
+    case Elixir.File.rm(@config_file) do
+      :ok ->
+        IO.puts("Cleared stored credentials")
+        :ok
+
+      {:error, _} ->
+        nil
+    end
+  end
+
+  defp save_credentials(username, password) when is_binary(username) and is_binary(password) do
+    data = %{username: username, password: password}
+
+    with {:exists, false} <- {:exists, Elixir.File.exists?(@config_file)},
+         true <- confirm?("Do you want to store your credential? (WARNING: they are stored in plain text) [y/n]"),
+         {:ok, encoded} <- Jason.encode(data),
+         :ok <- Elixir.File.write("config.json", encoded) do
+      :ok
+    else
+      {:exists, true} ->
+        :ok
+
+      {:error, reason} ->
+        IO.puts("Unable to save credentials: #{reason}")
+        :error
+    end
+  end
+
+  defp confirm?(prompt) when is_binary(prompt) do
+    answer = prompt |> IO.gets() |> String.trim() |> String.downcase()
+
+    case answer do
+      "y" -> true
+      "n" -> false
+      _ -> confirm?(prompt)
     end
   end
 

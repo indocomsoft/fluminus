45c9899d2b122b1445ac5830dcf6eb1d23014618
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-03-08 17:50:09 +0800

    Fix offences

--- test/fluminus/api_test.exs
+++ test/fluminus/api_test.exs
@@ -18,7 +18,7 @@ defmodule Fluminus.APITest do
   setup do
     bypass = Bypass.open(port: @port)
     Bypass.expect(bypass, &handle_conn/1)
-    :ok
+    {:ok, bypass: bypass}
   end
 
   defp handle_conn(conn) do
@@ -27,14 +27,7 @@ defmodule Fluminus.APITest do
     {:ok, body, conn} = Conn.read_body(conn)
     body = if body != "", do: URI.decode_query(body), else: %{}
 
-    cassette =
-      Enum.find(@cassettes, fn %{"request" => request} ->
-        request["request_path"] == conn.request_path and
-          request["method"] == conn.method and
-          (request["cookies"] || %{}) == conn.cookies and
-          (request["body"] || %{}) == body and
-          Enum.all?(request["query"] || %{}, fn {key, value} -> Map.get(conn.query_params, key) == value end)
-      end)
+    cassette = find_casette(conn, body)
 
     if cassette do
       response = cassette["response"]
@@ -45,12 +38,25 @@ defmodule Fluminus.APITest do
       end)
       |> Conn.resp(response["status_code"], response["body"])
     else
-      Logger.error("Please create cassette for this request: #{conn.method} #{conn.request_path}")
-      Logger.error("conn:")
-      Logger.error(inspect(conn))
-      Logger.error("body:")
-      Logger.error(inspect(body))
+      error(conn, body)
       conn
     end
+  end
+
+  defp find_casette(conn, body) do
+    Enum.find(@cassettes, fn %{"request" => request} ->
+      request["request_path"] == conn.request_path and
+        request["method"] == conn.method and
+        (request["cookies"] || %{}) == conn.cookies and
+        (request["body"] || %{}) == body and
+        Enum.all?(request["query"] || %{}, fn {key, value} -> Map.get(conn.query_params, key) == value end)
+    end)
+  end
+
+  defp error(conn, body) do
+    Logger.error("Please create cassette for this request: #{conn.method} #{conn.request_path}")
+    Logger.error("cookies: #{Jason.encode!(conn.cookies)}")
+    Logger.error("body: #{Jason.encode!(body)}")
+    Logger.error("query: #{Jason.encode!(conn.query_params || %{})}")
   end
 end
--- test/fluminus/authorization_test.exs
+++ test/fluminus/authorization_test.exs
@@ -31,8 +31,11 @@ defmodule Fluminus.AuthorizationTest do
     assert auth.client.cookies["idsrv"] == @idsrv
   end
 
-  test "jwt invalid credentials" do
+  test "jwt errors", %{bypass: bypass} do
     {:error, :invalid_credentials} = Authorization.jwt("e1234567", "wrongpassword")
+
+    Bypass.down(bypass)
+    {:error, %HTTPoison.Error{reason: :econnrefused}} = Authorization.jwt("e0123456", "hunter2")
   end
 
   test "renew_jwt happy path" do
@@ -49,14 +52,7 @@ defmodule Fluminus.AuthorizationTest do
     {:ok, body, conn} = Conn.read_body(conn)
     body = if body != "", do: URI.decode_query(body), else: %{}
 
-    cassette =
-      Enum.find(@cassettes, fn %{"request" => request} ->
-        request["request_path"] == conn.request_path and
-          request["method"] == conn.method and
-          (request["cookies"] || %{}) == conn.cookies and
-          (request["body"] || %{}) == body and
-          Enum.all?(request["query"] || %{}, fn {key, value} -> Map.get(conn.query_params, key) == value end)
-      end)
+    cassette = find_casette(conn, body)
 
     if cassette do
       response = cassette["response"]
@@ -67,11 +63,25 @@ defmodule Fluminus.AuthorizationTest do
       end)
       |> Conn.resp(response["status_code"], response["body"])
     else
-      Logger.error("Please create cassette for this request: #{conn.method} #{conn.request_path}")
-      Logger.error("cookies: #{Jason.encode!(conn.cookies)}")
-      Logger.error("body: #{Jason.encode!(body)}")
-      Logger.error("query: #{Jason.encode!(conn.query_params || %{})}")
+      error(conn, body)
       conn
     end
+  end
+
+  defp find_casette(conn, body) do
+    Enum.find(@cassettes, fn %{"request" => request} ->
+      request["request_path"] == conn.request_path and
+        request["method"] == conn.method and
+        (request["cookies"] || %{}) == conn.cookies and
+        (request["body"] || %{}) == body and
+        Enum.all?(request["query"] || %{}, fn {key, value} -> Map.get(conn.query_params, key) == value end)
+    end)
+  end
+
+  defp error(conn, body) do
+    Logger.error("Please create cassette for this request: #{conn.method} #{conn.request_path}")
+    Logger.error("cookies: #{Jason.encode!(conn.cookies)}")
+    Logger.error("body: #{Jason.encode!(body)}")
+    Logger.error("query: #{Jason.encode!(conn.query_params || %{})}")
   end
 end

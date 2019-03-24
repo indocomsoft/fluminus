b5ddf77db879c70ea98cf30346b410709c860914
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-03-24 11:29:02 +0800

    Bump version to 1.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fluminus.MixProject do
   def project do
     [
       app: :fluminus,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,

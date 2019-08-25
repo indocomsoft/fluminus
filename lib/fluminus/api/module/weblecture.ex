67b70384c2f62298a60bb09a098a06d3c2d70566
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-08-25 10:18:50 +0800

    Add moduledoc to Weblecture

--- lib/fluminus/api/module/weblecture.ex
+++ lib/fluminus/api/module/weblecture.ex
@@ -1,4 +1,14 @@
 defmodule Fluminus.API.Module.Weblecture do
+  @moduledoc """
+  Provides an abstraction over a weblecture in LumiNUS, and operations possible on them using]
+  LumiNUS API.
+
+  Struct fields:
+  * `:id` - id of the weblecture
+  * `:name` - name of the weblecture
+  * `:module_id` - the module_if to which the weblecture is from.
+  """
+
   defstruct ~w(module_id id name)a
 
   @type t :: %__MODULE__{module_id: String.t(), id: String.t(), name: String.t()}
